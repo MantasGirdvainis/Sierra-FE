5674b5a270fc39b219fd33e1531a72185869dc9c
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2022-10-16 19:46:39 +0300

    updated

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,11 @@
 import styles from './Layout.module.css';
-import Header from '../Header/header';
 
 type Props = { header: string | JSX.Element; children: string | JSX.Element; footer: string | JSX.Element };
 
-// eslint-disable-next-line
-export default function Layout({ children, header, footer }: Props) {
+export default function Layout({ children, header, footer }: Props): JSX.Element {
   return (
     <>
-      <Header />
+      {header}
       <main className={styles.marginTop}>{children}</main>
       <footer className={styles.horizontalMargin}>{footer}</footer>
     </>

71310a848299c92844d0f6318cacda4d0fd08d11
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2022-10-28 19:41:50 +0300

    updated

--- src/containers/MoviesListContainer/Movieslistcontainer.tsx
+++ src/containers/MoviesListContainer/Movieslistcontainer.tsx
@@ -1,17 +1,10 @@
-import { useQuery } from 'react-query';
-import { fetchStatus } from 'api/health';
-import { MyMoviesLogo } from 'components/Icons';
-
-import styles from './MoviesListContainer.module.css';
+import styles from './MoviesListContainer.module.css'
 
 const MoviesListContainer = (): JSX.Element => {
-  const { data: healthy } = useQuery('status', fetchStatus);
   return (
-    <>
-      <MyMoviesLogo className={styles.icon} />
-      <p>My Movies</p>
-      <p>API Status: {healthy ? 'Is running' : 'Something is wrong!'}</p>
-    </>
+    <div className={styles.movieListContainer}>
+      Movie list
+    </div>
   );
 };
 

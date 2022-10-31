df325ec62da1e1568c5b218865166cd677899e20
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2022-10-31 12:04:26 +0200

    movie request created

--- src/containers/MoviesListContainer/Movieslistcontainer.tsx
+++ src/containers/MoviesListContainer/Movieslistcontainer.tsx
@@ -1,11 +1,18 @@
-import styles from './MoviesListContainer.module.css'
+import { useQuery } from 'react-query';
+import { getMovies } from 'api/movies/movies';
+import Loader from 'components/Loader/loader';
+
+import styles from './MoviesListContainer.module.css';
 
 const MoviesListContainer = (): JSX.Element => {
-  return (
-    <div className={styles.movieListContainer}>
-      Movie list
-    </div>
-  );
+  const { data } = useQuery('movies', getMovies)
+  
+  if (data) {
+    return <div className={styles.movieListContainer}>{JSON.stringify(data)}</div>;
+  } 
+
+  return  <Loader />;
+ 
 };
 
 export default MoviesListContainer;

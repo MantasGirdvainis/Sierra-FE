ca54553a6c26d343737b7f9a4789b2843b149042
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2023-02-28 14:23:43 +0200

    provider created

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import Layout from 'components/Layout/Layout';
 import Header from 'components/Header/header';
 import { BrowserRouter } from 'react-router-dom';
 import MainRouter from 'navigation/MainRouter';
+import ProfileProvider from 'providers/ProfileProvider';
 
 import Footer from './components/Footer/footer';
 
 
 function App(): JSX.Element {
-  
+
   return (
     <BrowserRouter>
-      <Layout footer={<Footer />} header={<Header />}>
-        <MainRouter />
-      </Layout>
-    
+      <ProfileProvider>
+        <Layout footer={<Footer />} header={<Header />}>
+          <MainRouter />
+        </Layout>
+      </ProfileProvider>
     </BrowserRouter>
   );
 }
